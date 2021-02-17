16694324217b0103075078f9094c577027e63092
jorge manuel manuel <l37krew@gmail.com>
2021-02-17 19:02:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import random
+import time
+
+def mostrarIntroducción():
+ print('Estás en una tierra llena de dragones. Frente a tí')
+ time.sleep(2)
+ print('hay dos cuevas. En una de ellas, el dragón es generoso y')
+ time.sleep(2)
+ print('amigable y compartirá su tesoro contigo. El otro dragón')
+ time.sleep(2)
+ print('es codicioso y está hambriento, y te devorará inmediatamente.')
+ time.sleep(2)
+ print()
+
+def elegirCueva():
+ cueva = ''
+ while cueva != '1' and cueva != '2':
+  print('¿A qué cueva quieres entrar? (1 ó 2)')
+  cueva = input()
+
+ return cueva
+
+def explorarCueva(cuevaElegida):
+ print('Te aproximas a la cueva...')
+ time.sleep(2)
+ print('Es oscura y espeluznante...')
+ time.sleep(2)
+ print('¡Un gran dragon aparece súbitamente frente a tí! Abre sus fauces y...')
+ print()
+ time.sleep(2)
+
+ cuevaAmigable = random.randint(1, 2)
+
+ if cuevaElegida == str(cuevaAmigable):
+   print('¡¡Te regala su tesoro!')
+ else:
+   print('¡Te engulle de un bocado!')
+
+jugarDeNuevo = 'sí'
+while jugarDeNuevo == 'sí' or jugarDeNuevo == 's':
+
+  mostrarIntroducción()
+
+  númeroDeCueva = elegirCueva()
+
+  explorarCueva(númeroDeCueva)
+
+  print('¿Quieres jugar de nuevo? (sí o no)')
+  jugarDeNuevo = input()
